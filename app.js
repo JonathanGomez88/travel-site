591907831c25c961a09e145da057681b57771df9
Jongomez <jongomez@comcast.net>
2018-06-19 14:38:44 -0400

    if/else and hid departure date

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
+$("input[id='one-way']").on("click", function (event) {
+  $("#return-date").hide()
+})
 
+$("input[id='round-trip']").on("click", function (event) {
+  $("#return-date").show()
+})
 
 // Click handlers which takes input and runs it through apis
 $("#submit-btn").on("click", function (event) {
+
   event.preventDefault();
 
 var departureDate = $("#departure-date").val();
@@ -15,33 +22,37 @@ console.log(returnDate);
 console.log(departureIATA);
 console.log(destinationIATA);
 
-  var oneWayQueryURL = "api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=%20vA7jW9vS5DQj5MHWkavbCHddVJqDV47d&origin=" + departureIATA + "&destination=" + destinationIATA + "&departure_date=" + departureDate
+  var oneWayQueryURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=%20vA7jW9vS5DQj5MHWkavbCHddVJqDV47d&origin=" + departureIATA + "&destination=" + destinationIATA + "&departure_date=" + departureDate + "&number_of_results=10"
   console.log(oneWayQueryURL)
 
-  var roundTripQueryURL = "api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=%20vA7jW9vS5DQj5MHWkavbCHddVJqDV47d&origin=" + departureIATA + "&destination=" + destinationIATA + "&departure_date=" + departureDate + "&return_date=" + returnDate
+  var roundTripQueryURL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=%20vA7jW9vS5DQj5MHWkavbCHddVJqDV47d&origin=" + departureIATA + "&destination=" + destinationIATA + "&departure_date=" + departureDate + "&return_date=" + returnDate + "&number_of_results=10"
 
 console.log(roundTripQueryURL)
 
 
 
 // if else statement to determine one-way/round trip
+
+if($("input[id='round-trip']:checked").val()) {
+  $.ajax({
+    url: roundTripQueryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+}  else {
+  $.ajax({
+    url: oneWayQueryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+}
  
-// if(onewaychecked) {
-// $.ajax({
-//     url: oneWayQueryURL,
-//     method: "GET"
-//   }).then(function (response) {
-//     console.log(response);
-//   });
+
+
 // } else {
-//   $.ajax({
-//     url: roundTripQueryURL,
-//     method: "GET"
-//   }).then(function (response) {
-//     console.log(response);
-//   });
-// }
-// });
+
 
 // $("#").on("click", function () {
 
